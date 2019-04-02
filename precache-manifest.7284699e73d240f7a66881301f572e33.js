627c90e65b502ed682a52600beb6d2453b6c528a
Unknown <gabriele@rigoinvestment.com>
2019-04-02 20:05:30 +0200

    upgrade to react 2
    fixed several bugs

---
+++ precache-manifest.7284699e73d240f7a66881301f572e33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "eb9faf33fedbe3d63a62",
+    "url": "./static/js/main.fc6dc819.chunk.js"
+  },
+  {
+    "revision": "af7906693a4f82ca364c",
+    "url": "./static/js/2.2eb4fd64.chunk.js"
+  },
+  {
+    "revision": "eb9faf33fedbe3d63a62",
+    "url": "./static/css/main.b4a0b9e0.chunk.css"
+  },
+  {
+    "revision": "af7906693a4f82ca364c",
+    "url": "./static/css/2.9f1b2c7d.chunk.css"
+  },
+  {
+    "revision": "8ccb8d3f0a995bbdd36ae262b2884141",
+    "url": "./index.html"
+  }
+];
