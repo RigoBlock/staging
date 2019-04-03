0134a03ba61b290ee8474ccd83bddb3cb1a566fa
Unknown <gabriele@rigoinvestment.com>
2019-04-03 09:54:37 +0200

    fix metamask injection

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8c16f9e23f2416c672c070a2569fce63.js"
+  "./precache-manifest.b5fcbcdc49a0193c28a076fbbe688cf8.js"
 );
 
 workbox.clientsClaim();
